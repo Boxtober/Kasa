f19cc5482bb218c0638f2407142e5689e73f1254
sabrina <sabrina@verymountain.fr>
2024-10-28 10:50:23 +0100

    :hammer: Refactor component and add responsive

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import bannerImg from '../../assets/img/BANNERIMG.jpg'
+import React from "react";
+import bannerImg from "../../assets/img/BANNERIMG.jpg";
 import "./banner.scss";
 
 const Banner = () => {
     return (
         <div className="banner">
-            <img src={bannerImg} alt="" className='imgBanner' />
+            <img src={bannerImg} alt="" className="imgBanner" />
             <div className="overlay"></div>
-            <h1>
-                Chez vous, partout et ailleurs
-                {/* <a href="#" target='_blank'> </a> */}
-            </h1>
+            <h1>Chez vous, partout et ailleurs</h1>
         </div>
     );
 };
